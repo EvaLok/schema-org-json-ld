c945a91e182f7522bb7fd5c4ad557cd5d888f47b
eva <eva.aedea@gmail.com>
2024-06-13 19:36:39 +0200

    correct location of comment block

--- src/v1/Schema/QuantitativeValue.php
+++ src/v1/Schema/QuantitativeValue.php
@@ -4,15 +4,15 @@ namespace EvaLok\SchemaOrgJsonLd\v1\Schema;
 
 use EvaLok\SchemaOrgJsonLd\v1\TypedSchema;
 
-/**
- * @param $unitCode
- * unitCode ref: https://github.com/schemaorg/schemaorg/wiki/Using-UN-CEFACT-Codes
- * - kilogram: KGM
- * - US pound: LBR
- */
 class QuantitativeValue extends TypedSchema {
 	const A_SCHEMA_TYPE = 'QuantitativeValue';
 
+	/**
+	 * @param $unitCode
+	 * unitCode ref: https://github.com/schemaorg/schemaorg/wiki/Using-UN-CEFACT-Codes
+	 * - kilogram: KGM
+	 * - US pound: LBR
+	 */
 	function __construct(
 		public float $value,
 		public string $unitCode,
